9dca100bd40e0223767d2269a8c42b27a041582d
jacobshatzky <jacob.sipprellshatzky@hrsdc-rhdcc.gc.ca>
2022-01-05 11:47:35 -0500

    create,update,delete

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+
+var http = require("http").createServer(app);
 const port = 9999;
+
+app.use("/static", express.static("./static/"));
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.sendFile(__dirname + "/index.html");
+  //res.send("Hello, World!");
 });
+
 app.listen(port, function () {
   console.log(`Server listening on port ${port}`);
 });
